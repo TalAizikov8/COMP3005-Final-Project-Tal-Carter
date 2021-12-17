8f55fb27eae1a4b9bfb95728a30c909959817574
TalAizikov8 <61164990+TalAizikov8@users.noreply.github.com>
2021-12-17 13:55:21 -0500

    Add files via upload

--- SQL/bookstoreDDL.sql
+++ SQL/bookstoreDDL.sql
@@ -1,49 +1,108 @@
---making sure nothing is in any of the tables 
-delete from book_into_basket;
-delete from c_order;
-delete from book;
-delete from report;
-delete from contacts;
-delete from owner;
-delete from publisher;
-delete from customer;
-delete from bank_account;
-
---bank account generate id by themselfs and the default money is 10000
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
-insert into bank_account Default Values;
---making a customer and assigning the bank account to them
-insert into customer values (default, 10000000, 'Joey Dooda', '123 Main Street' , '123456789');
-insert into customer values (default, 10000001, 'Lissy Moody', '22  Hoews Circle' , '1234588888');
---making a publisher and assigning the bank account to them
-insert into publisher values (default, 10000002, 'Bossy Worldy', '1 Wall Street', 'Bossy@Wall.com', '1111111111');
-insert into publisher values (default, 10000003, 'Law Order', '1 Court Main', 'Law@order.com', '1212121212');
-insert into publisher values (default, 10000004, 'Smith Smith', '33 Holy Smith Street', 'smith@smith.com  ', '1212121212');
---making an owner and assigning the bank account to them
-insert into owner values (default, 10000005, 'Many Shop', '321 Store Ave', '3344334433');
-insert into owner values (default, 10000006, 'Rip Off', '321 Steal Street', '1569518222');
-insert into owner values (default, 10000007, 'Mafia Boss', '321 Gank Ave', '2224442425');
---making many books each book has a publisher and an owner
-insert into book values (default, 30000000, 40000000, 'The Most Interesting Book', 'Action','Inter Esting', 469, 15.99, 20, 0.23);
-insert into book values (default, 30000000, 40000000, 'The Least Interesting Book', 'Action','Inter Esting', 694, 20.99, 25, 0.15);
-insert into book values (default, 30000000, 40000000, 'A Moderately Interesting Book', 'Action', 'Inter Esting', 542, 17.99, 20, 0.31);
-insert into book values (default, 30000001, 40000000, 'How to Eat A Lot','Cooking', 'Fat Joe', 333, 55.99, 25, 0.17);
-insert into book values (default, 30000001, 40000000, 'Everything About Candles', 'Lifestyle', 'Free Ki', 15, 5.99, 30, 0.2);
-insert into book values (default, 30000000, 40000001, 'How to Stay Up', 'Lifestyle','Free Ki', 100, 19.99, 30, 0.3);
-insert into book values (default, 30000000, 40000001, 'The Universe and Beyond', 'Science', 'Buzz Lightyear', 980, 33.99, 25, 0.06);
-insert into book values (default, 30000001, 40000001, 'Databases for Dummies', 'Computer-Science','Day Tah',  777, 99.99, 50, 0.5);
---This is a table which shows which owner and publisher are in contact
-----If an owner has a book from a publisher they will be in contact, it also does that in the java
-insert into contact values (40000000, 30000000);
-insert into contact values (40000001, 30000000);
-insert into contact values (40000000, 30000001);
-insert into contact values (40000001, 30000001);
-
---Note some tables have not been inserted to. Specificaly report, c_order, book_into_basket
-----This is because no orders have been made. Orders should be made through the java (aka actual book store)
+--Creating a bank account
+create table bank_account
+	(b_id		integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 10000000), --starting at 10000000 and adding one for each next bank account to generate a new id
+	 money	numeric(15, 2) not null DEFAULT 10000, --cant have null money and each person we assume starts with 10000
+	 primary key (b_id) --the id is the primary key
+	);
+--Creating a customer
+----Can buy book and view books. Can also create a new customer.
+create table customer
+	(c_id		integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 20000000), --Here we start at 20000000 overlap is allowed but to make it easier to follow added 10000000 to different IDs
+	 b_id		integer,
+	 name		varchar(20) not null,
+	 address	varchar(30) not null,
+	 phone		varchar(10),
+	 primary key (c_id),
+	 foreign key (b_id) references bank_account (b_id) --bank id is a foreign key
+		on delete cascade
+	);
+--Creating a publisher
+---- gets a percent cut of each sale of their book
+create table publisher
+	(p_id		integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 30000000),
+	 b_id		integer,
+	 name		varchar(20) not null,
+	 address	varchar(30),
+	 email		varchar(30) not null,
+	 phone		varchar(10),
+	 primary key (p_id),
+	 foreign key (b_id) references bank_account (b_id)
+		on delete cascade
+	);
+--owns the books in the bookstore
+create table owner
+	(o_id		integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 40000000), 
+	 b_id		integer, 
+	 name		varchar(20) not null,
+	 address	varchar(30),
+	 phone		varchar(10),
+	 primary key (o_id),
+	 foreign key (b_id) references bank_account (b_id)
+		on delete cascade
+	);
+--Table for which owner contacts which publishers
+----a publisher can be in contact with 2 owners and an owner can be in contact with 2 publishers
+create table contacts
+	(o_id		integer,
+	 p_id		integer,
+	 primary key	(o_id, p_id),
+	 foreign key	(o_id) references owner (o_id)
+		on delete cascade,
+	 foreign key	(p_id) references publisher (p_id)
+		on delete cascade
+	);
+--Reports are generated daily at the end of each day.
+----They show expenses vs profit for that day
+----They get combined with other tables to generate full reports in the application
+create table report
+	(date		varchar(8),
+	 o_id		integer,
+	 expense_amount	numeric(15, 2),
+	 profit_amount		numeric(15, 2),
+	 primary key (date),
+	 foreign key (o_id) references owner (o_id)
+		on delete set null
+	);
+--The book table
+create table book
+	(ISBN		integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1111111111), --10 digit ISBN value like the old standard
+	 p_id		integer,
+	 o_id		integer,
+	 name		varchar(32) not null,--We cant create a book without a name, genre, author, etc..
+	 genre		varchar(16) not null,
+	 author		varchar(32) not null,
+	 pages		numeric(5,0) not null check (pages > 0), --Things like pages, price, inventory must be bigger than 0. 
+	 price		numeric(5,2) not null check (price > 0),
+	 inventory 	numeric(2,0) not null check (inventory >= 0), 
+	 percent_to_publisher	numeric(3,2) not null check (percent_to_publisher >= 0 and percent_to_publisher < 1.00), --percent is a fruction from 0 to 1. 
+	 primary key (ISBN),
+	 foreign key (o_id) references owner (o_id)
+		on delete cascade,
+	 foreign key (p_id) references publisher (p_id)
+		);
+--Keeps track of cutomer order
+----an order is created before they place it but the status will be in basket. They have the same order id until they actually place the order
+create table c_order
+	(order_id	integer GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 50000000), 
+	 c_id		integer, 
+	 Billing_address	varchar(30) not null DEFAULT '800 Factory Street',
+	 shipping_address	varchar(30) not null DEFAULT '800 Factory Street',
+	 status			varchar(20) not null DEFAULT 'In Basket',
+	 date			varchar(8) not null DEFAULT '00000000',
+	 price			numeric(6,2) not null DEFAULT 0,
+	 primary key (order_id),
+	 foreign key (c_id) references customer (c_id)
+		on delete cascade
+	);
+--Keeps track of all the things in basket.
+----The basket is kept even when the castomer logs out zzz
+create table book_into_basket
+	(order_id	integer,
+	 ISBN		integer,
+	 amount_sold	numeric(2, 0) not null,
+	 primary key (order_id, ISBN),
+	 foreign key (order_id) references c_order (order_id)
+		on delete cascade,
+foreign key (ISBN) references book (ISBN)
+	on delete cascade
+	);
