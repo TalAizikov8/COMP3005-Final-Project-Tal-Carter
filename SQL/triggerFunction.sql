88151ad50cc4c92995d4ef75c45824b2f33d5c0e
TalAizikov8 <61164990+TalAizikov8@users.noreply.github.com>
2021-12-19 18:40:30 -0500

    Update triggerFunction.sql

--- SQL/triggerFunction.sql
+++ SQL/triggerFunction.sql
@@ -1,30 +1,34 @@
 create function update_profit_fun() returns trigger as $update_profit$
-begin
-	WITH price_sum AS (
+begin 
+--The code below find the expense amount from book and book_into_basket
+--It sums the price*amount*percent to publisher of all the new book which have been added to an order
+	WITH price_sum AS ( 
 	SELECT SUM(sums.price*amount_sold*percent_to_publisher) as total
 	FROM (SELECT price, amount_sold, percent_to_publisher FROM (book NATURAL JOIN book_into_basket) WHERE order_id = new.order_id) as sums)
-	
+--updating expenses with the amount calculated above
 	update report
 	set expense_amount = expense_amount + price_sum.total from price_sum
 	where report.date = NEW.date;
---test
+--updating report based on the price of the new order
 	update report
-	set profit_amount = profit_amount + (select price from c_order where order_id = new.order_id)
+	set profit_amount = profit_amount + (select price from c_order where order_id = new.order_id) 
 	where report.date = NEW.date;
 	
 	return new;
 end;
 $update_profit$ language plpgsql;
-
+-- creating a trigger for the code above
 create trigger update_profit after update of price on c_order
 for each row execute function update_profit_fun();
 
 
 create function books_threshold_fun() returns trigger as $books_threshold$
 begin
+--when inventory is less than 5 then we order more inventory based on books sold in last month
     if (new.inventory < 5) then
     Update book
     set inventory = inventory + 
+    --select the amount of books sold based books which have been ordered last month and which match the new ISBN
     (select amount_sold
     from (select ISBN, amount_sold
         From (select CAST(SUBSTRING(date, 3,2) as int) as month, ISBN, amount_sold from
@@ -34,6 +38,7 @@ begin
 Where 
     s.month =
     ((SELECT  CAST(SUBSTRING(date, 3,2) as int) as month from
+      	--the query below find the latest date and subtracts 1
         (Select date
         From (Select order_id, date
             From c_order 
@@ -43,6 +48,6 @@ Where
 	return new;
 end;
 $books_threshold$ language plpgsql;
-
+--create trigger for the function above
 create trigger books_threshold after update of inventory on book
     for each row execute function books_threshold_fun();
